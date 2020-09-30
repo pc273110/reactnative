b7623a8fa1d9bdb35451f4b7d83495ba689278e5
pc273110 <30820743+pc273110@users.noreply.github.com>
2020-09-30 18:07:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
-import ComponentsExercise from "./src/screens/ComponentsExercise";
-
+import ComponentsExercise from "./src/screens/ListScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ListAge from "./src/screens/ListAge";
 // its a tool from a library called 'React Navigation' that is used to show different screens to the user
 const navigator = createStackNavigator(
   {
    // Home: HomeScreen,
     //Component: ComponentsScreen
-    Exercise: ComponentsExercise
+ //   Exercise: ComponentsExercise,
+    //List: ListScreen,   
+    Age: ListAge,
   },
   {
-    initialRouteName: "Exercise",
+    initialRouteName: "Age",
     defaultNavigationOptions: {
       title: "App"
     }
