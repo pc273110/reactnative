3e5da90714f42d50d888543ca1daee82f9b1245b
piyush <piyush_p@nid.edu>
2020-09-30 11:54:32 +0530

    master

--- App.js
+++ App.js
@@ -2,17 +2,18 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
-import ComponentsExercise from "./src/screens/ComponentsExercise";
-
+import ComponentsExercise from "./src/screens/ListScreen";
+import ListScreen from "./src/screens/ListScreen";
 // its a tool from a library called 'React Navigation' that is used to show different screens to the user
 const navigator = createStackNavigator(
   {
    // Home: HomeScreen,
     //Component: ComponentsScreen
-    Exercise: ComponentsExercise
+ //   Exercise: ComponentsExercise,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Exercise",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App"
     }
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ListScreen = () => {
+    return <Text>List Screen</Text>;
+};
+
+const stles = StyleSheet.create({});
+
+export default ListScreen;
